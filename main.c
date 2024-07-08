777256874d81804a2e7ea9b6cb0cab0aef265449
gopher <gopher9511@outlook.com>
2024-07-08 09:46:46 +0800

    2024.7.4

--- main.c
+++ main.c
@@ -26,16 +26,44 @@
 //
 //    return 0;
 //}
+
+//二进制显示
+void bin32bdis(int data)
+{
+    int i=32;
+    while(i--)     //32---1
+    {
+        if(data & (1<<i))   //31--0
+            printf("1");
+        else
+            printf("0");
+        if(i%4==0)
+        {
+            if(i%8==0)
+                printf(" ");
+            else
+                printf("-");
+        }
+    }
+    putchar(10);
+}
+
 int main(){
-    char a[100];
-    printf("请输入：");
-    scanf("%s", a);
-    char n[26]={0};
-    for (int i = 0; i <strlen(a) ; ++i) {
-         n[a[i]-97]++;
-    }
-    for (int i = 0; i <26 ; ++i) {
-        printf("%c : %d \n",i+97, n[i]);
-    }
+//    char a[100];
+//    printf("请输入：");
+//    scanf("%s", a);
+//    char n[26]={0};
+//    for (int i = 0; i <strlen(a) ; ++i) {
+//         n[a[i]-97]++;
+//    }
+//    for (int i = 0; i <26 ; ++i) {
+//        printf("%c : %d \n",i+97, n[i]);
+//    }
+    int num=123456;
+    printf("%d\n", strlen(&num));
+    printf("%d\n", sizeof(num));
+
+    bin32bdis(num);
+
     return 0;
 }
